0c8acd77a7979d5cacd439f17f707159973956f5
Vladyslav Pekker <agilesteel@gmail.com>
2020-01-26 10:40:19 -0300

    Update install-direnv.sh

--- install-direnv.sh
+++ install-direnv.sh
@@ -3,7 +3,7 @@
 # remove yourself
 rm $0
 
-FALLBACK_VERSION="2.20.0"
+FALLBACK_VERSION="2.21.1"
 VERSION="${1:-$FALLBACK_VERSION}"
 SOURCE=https://github.com/direnv/direnv/releases/download/v$VERSION/direnv.linux-amd64
 TARGET=/usr/local/bin/direnv
